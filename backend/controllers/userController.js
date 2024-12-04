08ccdc7af7fe6fe9dd1840c8498dd77e6b6bc641
Abstract-a <m.kamari100@gmail.com>
2024-12-04 20:27:32 +0100

    added user model, controller and routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.body.id);
+  const { _id, name, email } = await User.findById(req.user._id);
   res.status(200).json({ id: _id, name, email });
 });
 const generateJWTtoken = (id) =>
